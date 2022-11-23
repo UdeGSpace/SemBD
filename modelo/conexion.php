75d55711b7d44cb17b69d4c5aa414cb2eb8d8e6a
UdeGSpace <sergio.sanchez3940@alumnos.udg.mx>
2022-11-23 02:52:42 -0600

    Login

---
+++ modelo/conexion.php
@@ -0,0 +1,17 @@
+<?php
+    define("Host", '127.0.0.1');
+    define ("BD",'Uniformes');
+    define ("User_BD",'root');
+    define ("Pass_BD",'070519');
+
+	$con = new mysqli(Host,User_BD,Pass_BD,BD);
+	$con->set_charset("utf8");
+	if($con->connect_error){
+		die("Error conectando al servidor de BD");
+		exit();
+	}else{
+		echo"Conectado";
+	}
+	$con->close();
+	return $con;
+?>
